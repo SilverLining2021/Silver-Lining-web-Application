b2f1b3232d76d478d4910d8b377e5a532024dbe9
Mike <31572222+BlyatEm@users.noreply.github.com>
2021-09-29 21:19:22 -0400

    Update Application.kt
    NOTE: 
    Unsure if this is the proper branching procedure. Let me know if this is acceptable or if not, please advise. 
    This is just some test code to show the routing method..

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -3,10 +3,32 @@ package com.example
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import com.example.plugins.*
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.http.content.*
+import io.ktor.response.*
+import io.ktor.routing.*
 
-fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
-        configureRouting()
-        configureSecurity()
+// TEST ROUTING
+// using "get" method - parameter is the site directory
+// a couple of tests here get to understand how this works 
+fun Application.main(){
+    routing{
+        get("/"){
+            call.respondText("Hello SilverLinings!")
+        }
+        get("/test1"){
+            call.respondText("Test 1...")
+        }
+        get("/test2"){
+            call.respondText("Test 2...")
+        }
+    }
+}
+
+fun main(){
+    embeddedServer(Netty, port = 8080, host = "127.0.0.1"){
+        install(Routing)
+        main()
     }.start(wait = true)
 }
