103f6912d8bb22755825abd7639d6a1e7f9dddb5
Cameron Conn <cam@camconn.cc>
2021-10-13 17:59:30 -0500

    Sorry, but I broke the build. This fixes it.

--- src/test/kotlin/com/silverlining/ApplicationTest.kt
+++ src/test/kotlin/com/silverlining/ApplicationTest.kt
@@ -10,11 +10,12 @@ import io.ktor.request.*
 import kotlin.test.*
 import io.ktor.server.testing.*
 import com.silverlining.plugins.*
+import com.silverlining.routes.setupRoutes
 
 class ApplicationTest {
     @Test
     fun testRoot() {
-        withTestApplication({ configureRouting() }) {
+        withTestApplication({ setupRoutes() }) {
             handleRequest(HttpMethod.Get, "/").apply {
                 assertEquals(HttpStatusCode.OK, response.status())
                 assertEquals("Hello World!", response.content)
