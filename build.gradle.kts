3c0b277d9de5a11e89871a992ce66b409c5884a6
Cameron Conn <cam@camconn.cc>
2021-10-13 17:58:24 -0500

    Resolve merge conflict

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val pbkdf_version: String by project
 
 plugins {
     application
@@ -23,10 +24,12 @@ dependencies{
     implementation("io.ktor:ktor-server-core:$ktor_version")
     implementation("io.ktor:ktor-auth:$ktor_version")
     implementation("io.ktor:ktor-server-netty:$ktor_version")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.ktor:ktor-server-sessions:$ktor_version")
     implementation("io.ktor:ktor-jackson:$ktor_version")
     implementation("io.ktor:ktor-gson:$ktor_version")
 
+
+    implementation("ch.qos.logback:logback-classic:$logback_version")
 
     // https://mvnrepository.com/artifact/org.ktorm/ktorm-core
     implementation("org.ktorm:ktorm-core:3.4.1")
@@ -46,4 +49,9 @@ dependencies{
 
 
 
+    // https://github.com/m9aertner/PBKDF2 or https://mvnrepository.com/artifact/de.rtner/PBKDF2
+    implementation( "de.rtner:PBKDF2:$pbkdf_version" )
+
+    testImplementation("io.ktor:ktor-server-tests:$ktor_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlin_version")
 }
