f4503093b18f49adcb17403ffd79a245557b7bf1
Cameron Conn <cam@camconn.cc>
2021-10-26 21:36:54 -0500

    Make user id autoincrement, remove old SQL code

--- src/main/kotlin/com/silverlining/ddl/Users.kt
+++ src/main/kotlin/com/silverlining/ddl/Users.kt
@@ -5,7 +5,7 @@ import org.jetbrains.exposed.sql.Table
 // Represents the users of the SilverLining web application
 object Users: Table("UserTable") {
     // unique user id
-    val id = integer("id")
+    val id = integer("id").autoIncrement()
     // username
     val username = varchar("username", 64)
     // password hash, length of up to 128 bytes
